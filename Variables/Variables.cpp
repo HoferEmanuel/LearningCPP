111dac5e195e34582e3119f91f88e05401e59229
Emanuel Hofer <Emanuel.hofer1@gmail.com>
2023-10-12 09:31:18 +0200

    added variables

---
+++ Variables/Variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int num = 8;
+	std::cout << num << std::endl;
+	num = 20;
+	std::cout << num << std::endl;
+	std::cin.get(); //wait for the user to press ENTER.
+}
