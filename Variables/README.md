91bc56c70cf8e33892665fc6adc2f69dd108f991
Emanuel Hofer <Emanuel.hofer1@gmail.com>
2023-10-12 11:57:25 +0200

    update variables

--- Variables/README.md
+++ Variables/README.md
@@ -1,6 +1,9 @@
 # Variables
 
 ## unsigned:
-``` c++
-ùnsigned int num = 8
+
+> changes the variables max-range to be only positiv `which doubles the max of 2147483647`
+
+```
+unsigned int num = 8
 ```
--- Variables/Variables.cpp
+++ Variables/Variables.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
+	std::cout << "--- Int ---" << std::endl;
 	int num = 8;
 	std::cout << num << std::endl;
 	num = 20;
 	std::cout << num << std::endl;
+
+	std::cout << "--- char ---" << std::endl;
+	char ch = 'a';
+	std::cout << ch << std::endl;
+
 	std::cin.get(); //wait for the user to press ENTER.
 }
